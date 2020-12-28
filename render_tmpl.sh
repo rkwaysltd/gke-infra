e3e720e56cb0e18c43fc4837826abe2a6b0baf2f
Rafal Kupka <r.kupson@gmail.com>
2020-12-28 10:38:45 +0000

    This variables may be unset

--- render_tmpl.sh
+++ render_tmpl.sh
@@ -6,8 +6,5 @@ fi
 
 : ${PROJECT_ID:?}
 : ${TF_STATE_BUCKET:?}
-: ${GOOGLE_BACKEND_CREDENTIALS:?}
-: ${GOOGLE_APPLICATION_CREDENTIALS:?}
-: ${GOOGLE_ENCRYPTION_KEY:?}
 
 envsubst < ./state.tf.tmpl > ./state.tf
