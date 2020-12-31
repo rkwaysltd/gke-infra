c784f3a11dfd364c6615274f7e35c662bfa8db98
Rafal Kupka <r.kupson@gmail.com>
2020-12-31 09:27:14 +0000

    Enable k8s-db key version roatation and prevent accidental deletion

--- kms.tf
+++ kms.tf
@@ -7,6 +7,12 @@ resource "google_kms_key_ring" "db" {
 resource "google_kms_crypto_key" "db" {
   name     = "k8s-db"
   key_ring = google_kms_key_ring.db.self_link
+  # 30 days
+  rotation_period = "2592000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "google_kms_key_ring_iam_binding" "key_ring_db" {
