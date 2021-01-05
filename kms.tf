d5fc87a35156681cd4cec2e12c95fbcead25c4e4
Rafal Kupka <r.kupson@gmail.com>
2021-01-05 15:40:42 +0000

    KeyRing named after cluster
    Key for root disk encryption added

--- kms.tf
+++ kms.tf
@@ -1,12 +1,14 @@
-resource "google_kms_key_ring" "db" {
+# KeyRing for cluster
+resource "google_kms_key_ring" "gke" {
   location = var.region
-  name     = "k8s-db"
+  name     = var.name
   project  = var.project_id
 }
 
+# Database (Secrets) encryption key
 resource "google_kms_crypto_key" "db" {
   name     = "k8s-db"
-  key_ring = google_kms_key_ring.db.self_link
+  key_ring = google_kms_key_ring.gke.self_link
   # 30 days
   rotation_period = "2592000s"
 
@@ -16,7 +18,28 @@ resource "google_kms_crypto_key" "db" {
 }
 
 resource "google_kms_key_ring_iam_binding" "key_ring_db" {
-  key_ring_id = google_kms_key_ring.db.id
+  key_ring_id = google_kms_key_ring.gke.id
+  role        = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
+  members = [
+    "serviceAccount:service-${data.google_project.gke_project.number}@container-engine-robot.iam.gserviceaccount.com",
+  ]
+}
+
+# Root disk (for nodes) encryption key
+resource "google_kms_crypto_key" "root_disk" {
+  name     = "k8s-root-disk"
+  key_ring = google_kms_key_ring.gke.self_link
+  # 30 days
+  rotation_period = "2592000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_kms_key_ring_iam_binding" "key_ring_root_disk" {
+  key_ring_id = google_kms_key_ring.gke.id
   role        = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
 
   members = [
