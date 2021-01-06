d07c25ef56051de97b4edf92545dfa797e60d0b0
Rafal Kupka <r.kupson@gmail.com>
2021-01-06 15:44:55 +0000

    Kubernetes provider setup

--- providers.tf
+++ providers.tf
@@ -7,3 +7,10 @@ provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
+
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = module.gke.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
