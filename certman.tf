157ca2de41260d148f7ff2723360f43209408639
Rafal Kupka <r.kupson@gmail.com>
2021-01-14 17:55:49 +0000

    Add chart for cert-manager

--- certman.tf
+++ certman.tf
@@ -11,3 +11,12 @@ resource "kubernetes_namespace" "cert_manager" {
     name = "cert-manager"
   }
 }
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.1.0"
+  namespace  = kubernetes_namespace.cert_manager.metadata[0].name
+  skip_crds  = false
+}
