57a8ee0d53fab363d9369319607673c7756ac551
Rafal Kupka <r.kupson@gmail.com>
2021-01-18 08:32:04 +0000

    Set resources on cert-manager

--- certman.tf
+++ certman.tf
@@ -24,4 +24,52 @@ resource "helm_release" "cert_manager" {
     name  = "installCRDs"
     value = "true"
   }
+
+  set {
+    name  = "resources"
+    value = <<EOF
+{
+  limits: {
+    memory: '256Mi',
+    cpu: '200m',
+  },
+  requests: {
+    memory: '128Mi',
+    cpu: '100m',
+  },
+}
+EOF
+  }
+
+  set {
+    name  = "webhook.resources"
+    value = <<EOF
+{
+  limits: {
+    memory: '128Mi',
+    cpu: '200m',
+  },
+  requests: {
+    memory: '64Mi',
+    cpu: '100m',
+  },
+}
+EOF
+  }
+
+  set {
+    name  = "cainjector.resources"
+    value = <<EOF
+{
+  limits: {
+    memory: '128Mi',
+    cpu: '200m',
+  },
+  requests: {
+    memory: '64Mi',
+    cpu: '100m',
+  },
+}
+EOF
+  }
 }
