ec0e6ecab80ca0ad4431bb46bc3baf5505236c34
Rafal Kupka <r.kupson@gmail.com>
2021-01-20 09:31:00 +0000

    Fix resource limits

--- certman.tf
+++ certman.tf
@@ -25,51 +25,7 @@ resource "helm_release" "cert_manager" {
     value = "true"
   }
 
-  set {
-    name  = "resources"
-    value = <<EOF
-{
-  limits: {
-    memory: '256Mi',
-    cpu: '200m',
-  },
-  requests: {
-    memory: '128Mi',
-    cpu: '100m',
-  },
-}
-EOF
-  }
-
-  set {
-    name  = "webhook.resources"
-    value = <<EOF
-{
-  limits: {
-    memory: '128Mi',
-    cpu: '200m',
-  },
-  requests: {
-    memory: '64Mi',
-    cpu: '100m',
-  },
-}
-EOF
-  }
-
-  set {
-    name  = "cainjector.resources"
-    value = <<EOF
-{
-  limits: {
-    memory: '128Mi',
-    cpu: '200m',
-  },
-  requests: {
-    memory: '64Mi',
-    cpu: '100m',
-  },
-}
-EOF
-  }
+  values = [
+    file("certman-values.yaml")
+  ]
 }
