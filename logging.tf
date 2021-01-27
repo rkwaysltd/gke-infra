74827ff4b7c25beb7ce8aa3ed849723288ee9dab
Rafal Kupka <r.kupson@gmail.com>
2021-01-27 15:13:21 +0000

    Example: cert-manager logs (keep for 30 days)

--- logging.tf
+++ logging.tf
@@ -5,3 +5,24 @@ resource "google_logging_project_bucket_config" "default" {
   retention_days = var.logs_retention_days
   bucket_id      = "_Default"
 }
+
+# Example: cert-manager logs (keep for 30 days)
+resource "google_logging_project_bucket_config" "cert_manager" {
+  project        = var.project_id
+  location       = "global"
+  retention_days = 30
+  bucket_id      = "cert-manager"
+}
+
+resource "google_logging_project_sink" "cert_manager" {
+  name                   = "cert-manager"
+  destination            = "logging.googleapis.com/${google_logging_project_bucket_config.cert_manager.id}"
+  filter                 = "resource.type = k8s_container resource.labels.namespace_name=\"${kubernetes_namespace.cert_manager.metadata[0].name}\" "
+  unique_writer_identity = true
+}
+
+resource "google_logging_project_exclusion" "cert_manager" {
+  name        = "cert-manager"
+  description = "Exclude cert-manager namespace logs. Stored elsewhere."
+  filter      = "resource.type = k8s_container resource.labels.namespace_name=\"${kubernetes_namespace.cert_manager.metadata[0].name}\" "
+}
