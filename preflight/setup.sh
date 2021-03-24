c81b8990bbe1bba4523af982702504282f8e5bb0
Rafal Kupka <r.kupson@gmail.com>
2021-03-24 16:32:57 +0000

    Skip SA account and key creation if already exists
    This script should be able to add permissions to existing service accounts...

--- preflight/setup.sh
+++ preflight/setup.sh
@@ -4,6 +4,23 @@ set -eu
 D=$(dirname $0)
 GCLOUD_CONFIG=$(gcloud config configurations list --format=json | jq -re '.[] | select(.is_active == true) | .name')
 
+# create Terraform service account and key
+create_service_account() {
+    local project_id="$1"
+    local t="$2"
+
+    if  gcloud iam service-accounts list --format=json | \
+        jq -re '.[] | select(.displayName == "terraform") | "Service account `terraform` already exists. Skip SA create and keys create actions."'; then
+        return
+    fi
+
+    gcloud iam service-accounts create terraform \
+        --description="Terraform SA" \
+        --display-name="terraform"
+    gcloud iam service-accounts keys create ./terraform-sa-key-${t}.json \
+        --iam-account="terraform@$project_id.iam.gserviceaccount.com"
+}
+
 for t in dev prod; do
     vars="$D/../variables.${t}.tfvars.json"
 
@@ -18,13 +35,8 @@ for t in dev prod; do
         logging.googleapis.com \
         monitoring.googleapis.com
 
-    # create Terraform service account and key
     PROJECT_ID="$(jq -re .project_id < "$vars")"
-    gcloud iam service-accounts create terraform \
-        --description="Terraform SA" \
-        --display-name="terraform"
-    gcloud iam service-accounts keys create ./terraform-sa-key-${t}.json \
-        --iam-account="terraform@$PROJECT_ID.iam.gserviceaccount.com"
+    create_service_account "${PROJECT_ID}" "${t}"
 
     # grant necessary permissions
     # https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/tree/v12.3.0#configure-a-service-account
