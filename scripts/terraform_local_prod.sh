da52a47eec9f0151798b9eefeea7cbcadc601129
Rafal Kupka <r.kupson@gmail.com>
2021-03-29 15:02:00 +0100

    Add local Terraform script for prod env
    I wouldn't do it but it's needed for cluster priming workaround.
    Terraform really want's to be interactive (read from tty) with `-target` option
    - hard to do in Github runner...

---
+++ scripts/terraform_local_prod.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+if [ -e ./.secrets ]; then
+    set -a
+    . .secrets
+    set +a
+fi
+
+D=$(dirname $0)
+vars="${D}/../variables.prod.tfvars.json"
+PROJECT_ID=$(jq -re .project_id < "$vars") "${D}/../render_tmpl.sh"
+
+cmd="${1:?run as: $0 init|plan|apply|...}"
+shift
+
+# terraform init should be executed with empty TF_WORKSPACE
+if [ "${cmd}" = "init" ]; then
+    ws=""
+else
+    ws="dev"
+fi
+
+opts=
+case "${cmd}" in
+state)
+    # no -var-file for state command
+    ;;
+*)
+    opts="-var-file=$vars"
+esac
+
+set -a
+TF_WORKSPACE="$ws"
+GOOGLE_CREDENTIALS="${GOOGLE_CREDENTIALS_PROD:?}"
+TF_VAR_cloudflare_api_token="${CLOUDFLARE_API_TOKEN_PROD:-}"
+TF_VAR_letsencrypt_email="${LETSENCRYPT_EMAIL:-}"
+TF_VAR_cloudflare_api_email="${CLOUDFLARE_API_EMAIL:-}"
+TF_VAR_cloudflare_domain_list="${CLOUDFLARE_DOMAIN_LIST:-}"
+TF_VAR_cloudflare_domain_ingress_rr="${CLOUDFLARE_DOMAIN_INGRESS_RR:-}"
+set +a
+terraform "$cmd" $opts "$@"
