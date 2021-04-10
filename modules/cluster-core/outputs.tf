091bbe6225cf7772c86fb88197048a542f2c319c
Rafal Kupka <r.kupson@gmail.com>
2021-04-10 08:32:01 +0100

    Needed for kubernetes-core provider configuration
    This module needs kubernetes provider but it has to be a different one than the
    one suitable for other modules.  This patch adds output variables for this
    special provider configuration.

--- modules/cluster-core/outputs.tf
+++ modules/cluster-core/outputs.tf
@@ -12,3 +12,20 @@ output "storageclass_cmek_disk_encryption_key" {
   description = "The KMS key that should be used for PVs Encryption-at-Rest."
   value       = google_kms_crypto_key.sc_storageclass_cmek_disk.self_link
 }
+
+# Those values should not be used to configure kubernetes or helm providers
+# other than the special kubernetes provider needed by this module itself.
+output "endpoint" {
+  sensitive = true
+  value     = module.gke.endpoint
+}
+
+output "access_token" {
+  sensitive = true
+  value     = base64encode(data.google_client_config.default.access_token)
+}
+
+output "cluster_ca_certificate" {
+  sensitive = true
+  value     = module.gke.ca_certificate
+}
